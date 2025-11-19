6012fb9a24c737b50638fdf214f31074ce63b4fb
lilyzamora-19 <lily_zamora@brown.edu>
2025-11-19 14:04:19 -0500

    Update simulation.R

--- simulation.R
+++ simulation.R
@@ -1,14 +1,28 @@
 #' Simulating Bike Data
 #' 
 #' @description
-#' @param1 n number of runs we will do for the simulation
-#' @param2 s number of samples we will use in each simulation
-#' @param3 
+#' @param1 
 
 # parameters must be defined
+set.seed(123)
 
-simulation <- function(n, s) {
+simulation <- function() {
   
   #Generate a matrix to store our data results
-  matrix
-}
+  matrix = matrix(0, nrow = p, ncol = p)
+  for (row in 1:p) {
+    for (col in 1:p) {
+      cov_mat[row, col] = rho^(abs(row-col))
+    }
+  }
+  
+  #Generate "happiness" variable
+  #0 is unhappy (no bike), 1 is happy (there is a bike)
+  happy <- sample(c(0, 1), size = n, replace = TRUE)
+  
+  #Generate x
+  
+  
+}.
+
+#Poisson process is here in simulation
