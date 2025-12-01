c168fe57b66479a74e08e4c661ced1d2a9a4f2d4
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-01 00:25:03 -0500

    Update optimizing_bikes.R

--- optimization/optimizing_bikes.R
+++ optimization/optimizing_bikes.R
@@ -3,7 +3,9 @@
 #' @description This function will find the number of bikes that each station should start with
 #' @param1 sample_bike a data frame that has the start and end stations and times for each bike trip
 #' @param2 num_bikes a data frame that shows the number of bikes each station has
-#' @return This function returns a data frame with station, number of bikes, and number of unhappy customers
+#' @return This function returns a data frame with station, number of bikes, and zero unhappy customers
+
+source("~/GitHub/PHP1560-Week12Lab/optimization/unhappy_customers.R")
 
 optimization <- function(sample_bike, num_bikes) {
   
