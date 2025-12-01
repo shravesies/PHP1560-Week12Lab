a23eddc2d1661d9e435404f314349525700044f8
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-01 00:23:45 -0500

    Update trips_simulation.R

--- simulations/trips_simulation.R
+++ simulations/trips_simulation.R
@@ -5,6 +5,16 @@
 #' demand for the day
 #' @param2 placement input that tracks placement of bikes to optimize on
 
+# Give every station some arbitrary number of bikes to run the trip simulation
+# With so we can then optimize on happiness with demand.
+unique(arrival_rates$start_station)
+unique(arrival_rates$end_station)
+
+placement <- c("R" = 20, "5" = 10, "18" = 20, "21" = 10, "11" = 20,
+               "12" = 10, "16" = 20, "17" = 10, "13" = 20, "22" = 10,
+               "6" = 20, "7" = 10, "8" = 20, "9" = 10, "4" = 20, "23" = 10,
+               "10" = 20, "19" = 10, "14" = 20, "15" = 10, "2" = 20, "24" = 10)
+
 simulate_trips <- function(demand_sim, placement) {
   # Organize in sequential order (otherwise bikes may be out of time order)
   demand_sim <- demand_sim %>% 
@@ -23,13 +33,15 @@ simulate_trips <- function(demand_sim, placement) {
   # of completed trips
     if (bikes[start] > 0) {
       bikes[start] <- bikes[start] - 1
-      bikes[end] <- bikes[endd] + 1
+      bikes[end] <- bikes[end] + 1
       completed <- completed + 1
     }
   }
   
-  list(
+  as.data.frame(list(
     completed_trips = completed,
-    end_bikes = bikes
+    end_bikes = bikes)
   )
 }
+
+sim_trips <- simulate_trips(demand_sim, placement)
