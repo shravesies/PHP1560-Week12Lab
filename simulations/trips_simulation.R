b15267635de80d8652b4e90c135cab2a852c2cfc
lilyzamora-19 <lily_zamora@brown.edu>
2025-11-30 21:20:27 -0500

    Update trips_simulation.R

--- simulations/trips_simulation.R
+++ simulations/trips_simulation.R
@@ -1,6 +1,35 @@
 #' Simulate Bike Trips
 #' 
-#' @description 
-#' @param 
+#' @description Simulates bike trips over a day
+#' @param1 demand_sim a data frame input that holds our simulation for bike 
+#' demand for the day
+#' @param2 placement input that tracks placement of bikes to optimize on
 
-#
+simulate_trips <- function(demand_sim, placement) {
+  # Organize in sequential order (otherwise bikes may be out of time order)
+  demand_sim <- demand_sim %>% 
+    arrange(time)
+  
+  # Code for tracking the amount of completed trips, bikes available, and
+  # placement input
+  bikes <- placement
+  completed <- 0
+  
+  for (i in 1:nrow(demand_sim)) {
+    start <- demand_sim$origin[i]
+    end <- demand_sim$destination[i]
+    
+  # Code for if a bike can take a trip or not and then add it to the list
+  # of completed trips
+    if (bikes[start] > 0) {
+      bikes[start] <- bikes[start] - 1
+      bikes[end] <- bikes[endd] + 1
+      completed <- completed + 1
+    }
+  }
+  
+  list(
+    completed_trips = completed,
+    end_bikes = bikes
+  )
+}
